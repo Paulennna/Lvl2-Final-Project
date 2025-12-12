d8b8f8e26154ede03855bd82f7aa75e16fd024b5
Paulennna <mariapaula.mpgp55@gmail.com>
2025-12-11 22:01:46 -0500

    Add files via upload
    Updated version

---
+++ Lvl2-Final Project/dashboard.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('mouseenter', function() {
+        this.style.background = 'linear-gradient(135deg, rgba(255, 240, 255, 1), rgba(255, 200, 255, 1))';
+    });
+    
+    card.addEventListener('mouseleave', function() {
+        this.style.background = 'linear-gradient(135deg, rgba(255, 240, 255, 0.9), rgba(255, 220, 255, 0.9))';
+    });
+});
+
+
